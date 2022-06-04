608feb89ef2c3a10b86416471fb07a27c0570ff2
Alexander Farmakis <alexander@movact.de>
2022-06-04 13:22:24 +0200

    not done yet. iteration 5 - 8 missing

--- src/movies.js
+++ src/movies.js
@@ -1,26 +1,76 @@
 // The `movies` array from the file `src/data.js`.
-console.log('movies: ', movies);
+//console.log('movies: ', movies);
+
+const movies = require("./data");
 
 
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(arr) {
+  let sumDirectors = arr.map(function(movie){
+    return movie.director
+  })
+  return sumDirectors
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(arr) {
+  if(arr.length === 0) {
+    return 0
+  }
+  let moviesFromSteven = arr.filter(function(movie){
+    //console.log(movie.genre.indexOf('Drama'))
+
+    return (movie.director === 'Steven Spielberg' && movie.genre.indexOf('Drama') >= 0)
+  })
+  return moviesFromSteven.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(arr) {
+  if(!arr.length) {
+    return 0
+  }
+  let scoreSum = arr.reduce(function(prev, curr){
+    if(curr.score === '' || curr.score === undefined) {
+      curr.score = 0
+    }
+    return prev + curr.score
+  }, 0)
+  //return scoreSum /  arr.length
+  const avgScore = scoreSum /  arr.length
+  return Number(avgScore.toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(arr) {
+  let dramaMovies = arr.filter(function(obj){
+    if(obj.genre.includes('Drama')){
+      return true
+    }
+  })
+  return scoresAverage(dramaMovies)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(arr) {
+  arr.sort(function(a, b){
+     let sortedYears = [];
+     if(a.year === b.year) {
+       return sortedYears = a.year - b.year
+     }
+   })
+ };
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(arr) {
+  let sorted = ''
+  arr.filter(function(sort){
+    return sorted = sort.title
+  })
+  return sorted
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
